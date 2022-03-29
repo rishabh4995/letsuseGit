f6d563a99da02aac615fd5a462d2ea046568d189
Meghesh Shenoy <60775906+megheshshenoy3@users.noreply.github.com>
2022-03-29 23:51:55 +0530

    Update CreateScratchOrg.sh

--- scripts/CreateScratchOrg.sh
+++ scripts/CreateScratchOrg.sh
@@ -11,17 +11,33 @@ echo "3"
 echo $SCRATCH_ORG_ALIAS
 echo "4"
 
-if [ -z $DEV_HUB_URLlocal ]; then
-  echo "set default devhub user"
-  execute sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
-
-  echo "deleting old scratch org"
+if[ "$1" = "true" ]; then
   sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
+else
+  if [ -z $DEV_HUB_URLlocal ]; then
+      echo "set default devhub user"
+      execute sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
+      echo "deleting old scratch org"
+      sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
+  fi
+   
+  echo "Creating scratch ORG..."
+  execute sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 7
+
+  sfdx force:org:list --all
+
+  echo "Pushing changes to scratch org..."
+  execute sfdx force:source:push
+
+  # echo "Assigning permission"
+  # execute sfdx force:user:permset:assign -n {Permission Set}
+
+  echo "Make sure Org user is english"
+  sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US"
+
+  echo "Running apex tests..."
+  execute sfdx force:apex:test:run -l RunLocalTests -w 30
 fi
-
-
-
-
 # if [ -f "package.json" ]; then
 #   echo "Running jest tests"
 #   execute npm install
